7d06917e9def8b4325903502e365ccb5d7647e84
Oksana-Tnt <127055817+Oksana-Tnt@users.noreply.github.com>
2023-07-06 23:00:44 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,16 +3,19 @@ const inputEl = document.querySelector("#validation-input");
 inputEl.addEventListener("blur", onCheckValidInput);
 
 function onCheckValidInput (event){    
-    console.log (inputEl.getAttribute("data-length")); 
+   
+    if(event.currentTarget.value.length !== Number(event.currentTarget.getAttribute("data-length"))){
+       
+        event.currentTarget.classList.remove("valid");
+        event.currentTarget.classList.add("invalid");          
 
-    if(event.currentTarget.value.length !== Number(inputEl.getAttribute("data-length"))){
-        inputEl.classList.add("invalid");        
 
-
-    } else if (event.currentTarget.value.length === Number(inputEl.getAttribute("data-length")))
+    } else if (event.currentTarget.value.length === Number(event.currentTarget.getAttribute("data-length")))    
     {
-        inputEl.classList.add("valid");
-        
-    }                 
+        event.currentTarget.classList.remove("invalid");
+        event.currentTarget.classList.add("valid");
+       
+    }      
+    
       
 };
