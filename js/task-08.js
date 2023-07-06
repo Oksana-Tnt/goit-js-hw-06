a6bb383d100dfc6bc75c1dc8890a28db6ff870d4
Oksana-Tnt <127055817+Oksana-Tnt@users.noreply.github.com>
2023-07-06 23:01:08 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
 
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", onForm);
+
+function onForm(event){
+    event.preventDefault();
+
+    const {email, password} = event.currentTarget.elements;
+
+    if(email.value === "" || password.value === ""){
+        alert ("can't be empty field!");
+    }
+
+    const data = {
+        email: email.value,
+        password: password.value,
+    };
+
+    console.log(data); 
+    console.dir(event);
+}
