ec34787c3e514c54163b9c69de27f685460dedc0
Oksana-Tnt <oksanatantsiura@gmail.com>
2023-07-07 18:32:31 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const refs = {
+  bodyEl: document.querySelector("body"),
+  buttonEl: document.querySelector(".change-color"),
+  spanEl: document.querySelector(".color"),
+};
+
+refs.buttonEl.addEventListener("click", onChangeColor)
+
+function onChangeColor(){
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  refs.spanEl.textContent = getRandomHexColor();
+};
