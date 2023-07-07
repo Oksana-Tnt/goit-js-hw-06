ffc75ecb8058435423d94c5a6efd7cee828a4d58
Oksana-Tnt <127055817+Oksana-Tnt@users.noreply.github.com>
2023-07-07 22:56:57 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  divControlsEl : document.querySelector("#controls"),
+  divBoxesEl : document.querySelector("#boxes"),
+  createBtnEl : document.querySelector('button[data-create]'),
+  destroyBtnEl : document.querySelector('button[data-destroy]'),  
+};
+
+
+refs.createBtnEl.addEventListener("click", onCreateBoxes);
+refs.destroyBtnEl.addEventListener("click", onDestroyBoxes);
+
+
+function onCreateBoxes(event){
+ let amount = refs.divControlsEl.firstElementChild.value;  
+  refs.divBoxesEl.insertAdjacentHTML("beforeend", createBoxes(amount));
+};
+
+function createBoxes(amount){  
+  let markup = "";
+  for ( let i=1, j=30; i<=amount; i+=1, j+=10){
+   markup += `<div style="background-color:${getRandomHexColor()}; width : ${j}px; height : ${j}px">hi</div>`;
+  }
+ return markup;
+};
+
+function onDestroyBoxes(){
+  refs.divBoxesEl.removeEventListener("click", onCreateBoxes);
+}
