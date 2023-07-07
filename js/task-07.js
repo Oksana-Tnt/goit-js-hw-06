e3ef16b4bbc96ef0ef8eaac2ebbd84060b6cee72
Oksana-Tnt <127055817+Oksana-Tnt@users.noreply.github.com>
2023-07-07 22:56:40 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,10 +2,7 @@ const inputEl=document.querySelector("#font-size-control");
 const spanEl=document.querySelector("#text");
 
 inputEl.addEventListener("input", onChangeInlineStyle);
-let step = 16;
-function onChangeInlineStyle(){
-    step +=1;
-    
-        spanEl.style.fontSize = `${step}px`;
-    
+
+function onChangeInlineStyle(event){
+   spanEl.style.fontSize = event.currentTarget.value + "px";    
 }
